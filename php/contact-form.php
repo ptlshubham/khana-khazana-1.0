cb76270b923dd0654d7851a8b2bdfbcef054d857
patelhet20 <het.patel5700@gmail.com>
2025-03-04 11:18:42 +0530

    all changes contact-form

--- php/contact-form.php
+++ php/contact-form.php
@@ -1,105 +1,367 @@
-<?php
-/*
-Name: 			Contact Form
-Written by: 	Okler Themes - (http://www.okler.net)
-Theme Version:	12.0.0
-*/
+<!DOCTYPE html>
+<html lang="en">
 
-namespace PortoContactForm;
+<head>
 
-session_cache_limiter('nocache');
-header('Expires: ' . gmdate('r', 0));
+	<!-- Basic -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-header('Content-type: application/json');
+	<title>Contact Khana Khazana | Indian Restaurant in USA</title>
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+	<meta name="keywords"
+		content="contact Indian restaurant, Khana Khazana location, Indian restaurant near me, best Indian food USA, order Indian food, restaurant hours" />
+	<meta name="description"
+		content="Get in touch with Khana Khazana! Visit us for an authentic Indian dining experience or order online. Find our location, opening hours, and contact details here.">
+	<meta name="author" content="Khana Khazana">
 
-require 'php-mailer/src/PHPMailer.php';
-require 'php-mailer/src/SMTP.php';
-require 'php-mailer/src/Exception.php';
+	<!-- Favicon -->
+	<meta property="og:image" content="https://khanakhazananj.com/img/apple-touch-icon.png">
+	<meta property="og:url" content="https://khanakhazananj.com/img/apple-touch-icon.png">
 
-// Step 1 - Enter your email address below.
-$email = 'you@domain.com';
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
 
-// If the e-mail is not working, change the debug option to 2 | $debug = 2;
-$debug = 0;
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
 
-// If contact form don't has the subject input change the value of subject here
-$subject = ( isset($_POST['subject']) ) ? $_POST['subject'] : 'Define subject in php/contact-form.php line 29';
+	<!-- Web Fonts  -->
+	<link id="googleFonts"
+		href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CPlayfair+Display:400,400i,700%7CSintony:400,700,800&display=swap"
+		rel="stylesheet" type="text/css">
 
-$message = '';
+	<!-- Vendor CSS -->
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+	<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+	<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
 
-foreach($_POST as $label => $value) {
-	$label = ucwords($label);
+	<!-- Theme CSS -->
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/theme-elements.css">
+	<link rel="stylesheet" href="css/theme-blog.css">
+	<link rel="stylesheet" href="css/theme-shop.css">
 
-	// Use the commented code below to change label texts. On this example will change "Email" to "Email Address"
+	<!-- Demo CSS -->
+	<link rel="stylesheet" href="css/demos/demo-church.css">
 
-	// if( $label == 'Email' ) {               
-	// 	$label = 'Email Address';              
-	// }
+	<!-- Skin CSS -->
+	<link id="skinCSS" rel="stylesheet" href="css/skins/skin-church.css">
 
-	// Checkboxes
-	if( is_array($value) ) {
-		// Store new value
-		$value = implode(', ', $value);
-	}
+	<!-- Theme Custom CSS -->
+	<link rel="stylesheet" href="css/custom.css">
 
-	$message .= $label.": " . nl2br(htmlspecialchars($value, ENT_QUOTES)) . "<br>";
-}
+</head>
 
-$mail = new PHPMailer(true);
+<body>
 
-try {
+	<div class="body">
+		<header id="header" class="header-effect-shrink"
+			data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': false, 'stickyStartAt': 120, 'stickyHeaderContainerHeight': 100}">
+			<div class="header-body border-top-0">
+				<div class="header-top">
+					<div class="container">
+						<div class="row justify-content-between">
+							<div class="col-auto">
+								<div class="d-flex align-items-center w-100 w-sm-50pct w-md-100pct ">
+									<div
+										class="animated-icon animated fadeIn svg-fill-color-primary position-relative bottom-2">
+										<!--?xml version="1.0" ?--><svg viewBox="0 0 512 512"
+											xmlns="http://www.w3.org/2000/svg" id="icon_71738931244817"
+											data-filename="phone.svg" width="27" height="27">
+											<title></title>
+											<g data-name="1" id="_1">
+												<path
+													d="M348.73,450.06a198.63,198.63,0,0,1-46.4-5.85c-52.43-12.65-106.42-44.74-152-90.36s-77.71-99.62-90.36-152C46.65,146.75,56.15,99.61,86.69,69.07l8.72-8.72a42.2,42.2,0,0,1,59.62,0l50.11,50.1a42.18,42.18,0,0,1,0,59.62l-29.6,29.59c14.19,24.9,33.49,49.82,56.3,72.63s47.75,42.12,72.64,56.31L334.07,299a42.15,42.15,0,0,1,59.62,0l50.1,50.1a42.16,42.16,0,0,1,0,59.61l-8.73,8.72C413.53,439,383.73,450.06,348.73,450.06ZM125.22,78a12,12,0,0,0-8.59,3.56l-8.73,8.72c-22.87,22.87-29.55,60-18.81,104.49,11.37,47.13,40.64,96.1,82.41,137.86s90.73,71,137.87,82.41c44.5,10.74,81.61,4.06,104.48-18.81l8.72-8.72a12.16,12.16,0,0,0,0-17.19l-50.09-50.1a12.16,12.16,0,0,0-17.19,0l-37.51,37.51a15,15,0,0,1-17.5,2.72c-30.75-15.9-61.75-39.05-89.65-66.95s-51-58.88-66.94-89.63a15,15,0,0,1,2.71-17.5l37.52-37.51a12.16,12.16,0,0,0,0-17.19l-50.1-50.11A12.07,12.07,0,0,0,125.22,78Z">
+												</path>
+												<path
+													d="M364.75,269.73a15,15,0,0,1-15-15,99.37,99.37,0,0,0-99.25-99.26,15,15,0,0,1,0-30c71.27,0,129.25,58,129.25,129.26A15,15,0,0,1,364.75,269.73Z">
+												</path>
+												<path
+													d="M428.15,269.73a15,15,0,0,1-15-15c0-89.69-73-162.66-162.65-162.66a15,15,0,0,1,0-30c106.23,0,192.65,86.43,192.65,192.66A15,15,0,0,1,428.15,269.73Z">
+												</path>
+											</g>
+										</svg>
+									</div>
+									<a class="text-color-primary text-decoration-none font-weight-bold text-2-5 opacity-hover-9 ms-2"
+										href="tel:+1 732 366 9116" target="_blank">+1 732 366 9116</a>
+								</div>
+							</div>
+							<div class="col-auto d-flex align-items-center">
+								<nav>
+									<ul class="list list-unstyled d-flex text-1 mb-0">
+										<li class="mb-0"><a href="contact-us.html#talkus"
+												class="alternative-font-4 text-decoration-none text-3-5 font-weight-bold">RESERVE
+												NOW</a></li>
 
-	$mail->SMTPDebug = $debug;                                 // Debug Mode
+									</ul>
+								</nav>
+								<div class="header-nav-features d-none d-md-block ms-5-5">
+									<ul
+										class="header-social-icons social-icons social-icons-clean social-icons-icon-dark social-icons-medium me-2 mb-0 d-none d-sm-block ms-1">
+										<li class="social-icons-instagram">
+											<a href="https://www.instagram.com/khana_khazana1760/" target="_blank"
+												title="Instagram"><i class="fab fa-instagram"></i></a>
+										</li>
+										<li class="social-icons-facebook">
+											<a href="https://www.facebook.com/profile.php?id=61565031478867"
+												target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="header-container container">
+					<div class="header-row">
+						<div class="header-column">
+							<div class="header-row">
+								<div class="header-logo">
+									<a href="index.html">
+										<img alt="Khana Khazana" width="224" height="90" src="img/logo.png">
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="header-column justify-content-end">
+							<div class="header-row">
+								<div class="header-nav order-2 order-lg-1 alternative-font-4">
+									<div
+										class="header-nav-main header-nav-main-square header-nav-main-dark-text header-nav-main-effect-1 header-nav-main-sub-effect-1">
+										<nav class="collapse">
+											<ul class="nav nav-pills" id="mainNav">
+												<li>
+													<a class="nav-link" href="index.html">
+														Home
+													</a>
+												</li>
+												<li>
+													<a class="nav-link" href="about-us.html">
+														About US
+													</a>
+												</li>
+												<li>
+													<a class="nav-link" href="catering.html">
+														Catering
+													</a>
+												</li>
+												<li>
+													<a class="nav-link" href="menu.html">
+														Menu
+													</a>
+												</li>
+												<li>
+													<a class="nav-link" href="gallery.html">
+														Gallery
+													</a>
+												</li>
+												<li>
+													<a class="nav-link active" href="contact-us.html">
+														Contact Us
+													</a>
+												</li>
 
-	// Step 2 (Optional) - If you don't receive the email, try to configure the parameters below:
+											</ul>
+										</nav>
+									</div>
+									<button class="btn header-btn-collapse-nav" data-bs-toggle="collapse"
+										data-bs-target=".header-nav-main nav">
+										<i class="fas fa-bars"></i>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
 
-	//$mail->IsSMTP();                                         // Set mailer to use SMTP
-	//$mail->Host = 'mail.yourserver.com';				       // Specify main and backup server
-	//$mail->SMTPAuth = true;                                  // Enable SMTP authentication
-	//$mail->Username = 'user@example.com';                    // SMTP username
-	//$mail->Password = 'secret';                              // SMTP password
-	//$mail->SMTPSecure = 'tls';                               // Enable encryption, 'ssl' also accepted
-	//$mail->Port = 587;   								       // TCP port to connect to
+		<div role="main" class="main">
 
-	$mail->AddAddress($email);	 						       // Add another recipient
+			<section
+				class="page-header page-header-modern page-header-background page-header-background-sm parallax overlay overlay-color-dark overlay-show overlay-op-1 my-0"
+				data-plugin-parallax data-plugin-options="{'speed': 1.5}" data-image-src="img/slider/h4.jpg">
+				<div class="container">
+					<div class="row my-5">
+						<div class="col-md-12 align-self-center text-center">
+							<h1 class="text-9 text-color-light custom-secondary-font font-weight-bold mb-1">Contact Us
+							</h1>
+							<p class="text-color-light custom-secondary-font text-uppercase mb-0">Get in Touch</p>
+						</div>
+					</div>
+				</div>
+			</section>
 
-	//$mail->AddAddress('person2@domain.com', 'Person 2');     // Add a secondary recipient
-	//$mail->AddCC('person3@domain.com', 'Person 3');          // Add a "Cc" address. 
-	//$mail->AddBCC('person4@domain.com', 'Person 4');         // Add a "Bcc" address. 
 
-	// From - Name
-	$fromName = ( isset($_POST['name']) ) ? $_POST['name'] : 'Website User';
-	$mail->SetFrom($email, $fromName);
+			<section id="talkus" class="section section-no-border bg-color-light m-0">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 mb-4 mb-md-0">
+							<h2 class="font-weight-bold">Talk to Us</h2>
+							<p>Get in touch with Khana Khazana for inquiries, reservations, or assistance—we're happy to
+								help!</p>
+							<form id="contactFormMessage" class="contact-form custom-form-style-1 mt-4"
+								action="php/contact-form.php" method="POST">
+								<h6 class="font-weight-bold custom-primary-font">Send a Message:</h6>
+								<div class="contact-form-success alert alert-success d-none mt-4"
+									id="contactFormMessageSuccess">
+									<strong>Success!</strong> Your message has been sent to us.
+								</div>
 
-	// Reply To
-	if( isset($_POST['email']) && !empty($_POST['email']) ) {
-		$mail->AddReplyTo($_POST['email'], $fromName);
-	}
+								<div class="contact-form-error alert alert-danger d-none mt-4"
+									id="contactFormMessageError">
+									<strong>Error!</strong> There was an error sending your message.
+									<span class="mail-error-message text-1 mt-2 d-block"
+										id="contactFormMessageErrorMessage"></span>
+								</div>
 
-	$mail->IsHTML(true);                                       // Set email format to HTML
+								<input type="hidden" name="subject" value="Church - Subject" />
+								<div class="row mb-3">
+									<div class="form-group col-sm-12">
+										<input type="text" value="" data-msg-required="Please enter your name."
+											maxlength="100" class="form-control custom-xs-mb" name="name" id="name"
+											placeholder="Your Name" required>
+									</div>
 
-	$mail->CharSet = 'UTF-8';
+								</div>
+								<div class="row mb-3">
+									<div class="form-group col-sm-6">
+										<input type="email" value=""
+											data-msg-required="Please enter your email address."
+											data-msg-email="Please enter a valid email address." maxlength="100"
+											class="form-control" name="email" id="email" placeholder="Your E-mail"
+											required>
+									</div>
+									<div class="form-group col-sm-6">
+										<input type="text" value="" data-msg-required="Please enter your Contact Number"
+											data-msg-email="Please enter a valid Contact Number" maxlength="100"
+											class="form-control" name="number" id="number"
+											placeholder="Your Contact Number" required>
+									</div>
+								</div>
+								<div class="row mb-3">
+									<div class="form-group col">
+										<textarea maxlength="5000" data-msg-required="Please enter your message."
+											rows="4" class="form-control" name="message" id="message"
+											placeholder="Your Message" required></textarea>
+									</div>
+								</div>
+								<div class="row mb-3">
+									<div class="form-group col">
+										<input type="submit" value="Send Message"
+											class="btn btn-primary custom-btn-style-1 text-uppercase float-end"
+											data-loading-text="Loading...">
+									</div>
+								</div>
+							</form>
+						</div>
+						<div class="col-lg-6">
+							<div class="row">
+								<h2 class="font-weight-bold">A Wonderful Dining Destination!</h2>
+								<p>Experience the delightful flavors of Khana Khazana, where delicious cuisine, warm
+									hospitality, and local charm come together for an unforgettable meal. We can't wait
+									to serve you!</p>
+								<iframe
+									src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3032.3007751726796!2d-74.5098097235933!3d40.53494364843547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3c100431e613f%3A0x940c38fae07be2c6!2sKhana%20Khazana!5e0!3m2!1sen!2sin!4v1739339472867!5m2!1sen!2sin"
+									width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+									referrerpolicy="no-referrer-when-downgrade"></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 
-	$mail->Subject = $subject;
-	$mail->Body    = $message;
+			<footer id="footer" class="bg-color-secondary custom-footer m-0">
+				<div class="container pt-5">
+					<div class="row text-center">
+						<div class="col">
+							<a href="index.html" class="text-decoration-none">
+								<img src="img/logo-footer.png" width="213" height="163" alt="Footer Logo"
+									class="img-fluid custom-img-fluid-center" />
+							</a>
+						</div>
+					</div>
+					<hr class="solid tall custom-hr-color-1">
+					<div class="row justify-content-center text-center">
+						<div class="col-lg-3 custom-sm-margin-bottom-1">
+							<i class="fas fa-map-marker-alt text-color-primary custom-icon-size-1"></i>
+							<p class="custom-text-color-2 alternative-font-4 text-3-5">
+								<strong
+									class="d-block text-color-light custom-secondary-font text-5-5 line-height-8 mb-1">Address</strong>
+								1760 Easton Ave, Somerset, </br> NJ 08873, United States
+							</p>
+						</div>
+						<div class="col-lg-4 custom-sm-margin-bottom-1">
+							<i class="far fa-clock text-color-primary custom-icon-size-1"></i>
+							<p class="custom-text-color-2 alternative-font-4 text-3-5">
+								<strong
+									class="d-block text-color-light custom-secondary-font text-5-5 line-height-8 mb-1">
+									Join us any day!
+								</strong>
+								Open Daily <br>
+								10:00 AM - 10:00 PM
+							</p>
+						</div>
+						<div class="col-lg-3">
+							<i class="fas fa-phone-volume text-color-primary custom-icon-size-1"></i>
+							<p class="alternative-font-4 text-3-5">
+								<strong
+									class="d-block text-color-light custom-secondary-font text-5-5 line-height-8 mb-1">Call
+									us now</strong>
+								<a href="tel:+1 732 366 9116" class="text-decoration-none custom-text-color-2">Phone :
+									+1 732 366 9116</a></br>
+								<a href="https://mail.google.com/mail/?view=cm&to=khanakhazananj@gmail.com"
+									target="_blank" class="text-decoration-none custom-text-color-2">
+									Email: khanakhazananj@gmail.com
+								</a>
+							</p>
+						</div>
+					</div>
+					<hr class="solid tall custom-hr-color-1">
+					<div class="row text-center pb-5">
+						<div class="col">
+							<ul class="social-icons social-icons-clean custom-social-icons mb-3">
+								<li class="social-icons-facebook"><a
+										href="https://www.facebook.com/profile.php?id=61565031478867" target="_blank"
+										title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+								<li class="social-icons-instagram"><a
+										href="https://www.instagram.com/khana_khazana1760/" target="_blank"
+										title="Instagram"><i class="fab fa-instagram"></i></a></li>
+							</ul>
+							<p class="alternative-font-4 text-3 text-color-light opacity-7">Copyright © 2025 Khana
+								Khazana Made By <a href="https://keryar.com/"> KERYAR.</a></p>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+		<!-- Vendor -->
+		<script src="vendor/plugins/js/plugins.min.js"></script>
+		<script src="vendor/jquery.countdown/jquery.countdown.min.js"></script>
 
-	// Step 3 - If you don't want to attach any files, remove that code below
-	if (isset($_FILES['attachment']) && $_FILES['attachment']['error'] == UPLOAD_ERR_OK) {
-		$mail->AddAttachment($_FILES['attachment']['tmp_name'], $_FILES['attachment']['name']);
-	}
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
 
-	$mail->Send();
-	$arrResult = array ('response'=>'success');
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
 
-} catch (Exception $e) {
-	$arrResult = array ('response'=>'error','errorMessage'=>$e->errorMessage());
-} catch (\Exception $e) {
-	$arrResult = array ('response'=>'error','errorMessage'=>$e->getMessage());
-}
+		<!-- Demo -->
+		<script src="js/demos/demo-church.js"></script>
 
-if ($debug == 0) {
-	echo json_encode($arrResult);
-}
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+		<!-- Google Maps -->
+
+
+</body>
+
+</html>
